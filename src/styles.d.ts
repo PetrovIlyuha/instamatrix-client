fb7d64baa4a28e9ec9403b3f23d4a273b8ae2ec1
Ilya <dnamix1@gmail.com>
2021-04-09 14:02:36 +0300

    custom declared module for styled-components theme

---
+++ src/styles.d.ts
@@ -0,0 +1,14 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    background: string;
+    color: string;
+    toggleBorder: string;
+    gradient: string;
+    darkTheme: boolean;
+    logoColor: string;
+    boxShadowColor: string;
+    inputBgColor: string;
+  }
+}
