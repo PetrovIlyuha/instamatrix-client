c1819f70d3862f2fce52728d7ef0c8241df3e07e
Ilya <dnamix1@gmail.com>
2021-04-09 14:01:32 +0300

    add main App.js later will be rewritten for Typescript

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import { useReactiveVar } from '@apollo/client';
 import React from 'react';
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { darkMode, loggedIn } from './apollo';
+import { GlobalStyles } from './GlobalStyles';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import SignUp from './screens/SignUp';
+import { darkTheme, lightTheme } from './themes.ts';
 
 function App() {
-  return <div className='App'>InstaMatrix</div>;
+  const isLogged = useReactiveVar(loggedIn);
+  const isDarkMode = useReactiveVar(darkMode);
+  return (
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <Router>
+        <Switch>
+          <Route path='/' exact>
+            {isLogged ? <Home /> : <Login />}
+          </Route>
+          <Route path='/signup'>
+            <SignUp />
+          </Route>
+          <Route>
+            <Redirect to='/' />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
