cbb3b428f5ba0fd3fa07d7dc33f9fe0395543225
Ilya <dnamix1@gmail.com>
2021-04-11 12:09:21 +0300

    add spinner with styled-components

--- src/reusable/styles/Shared.js
+++ src/reusable/styles/Shared.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
@@ -8,4 +8,22 @@ export const Container = styled.div`
   height: 100vh;
   width: 100vw;
   align-items: center;
+`;
+
+export const spin = keyframes`
+  0% {
+    transform: rotate(0deg)
+  }
+  360% {
+    transform: rotate(360deg)
+  }
+`;
+
+export const Spinner = styled.div`
+  width: ${({ size }) => size};
+  height: ${({ size }) => size};
+  text-align: center;
+  border: 2px solid ${({ color }) => color};
+  border-radius: 50%;
+  animation: 2s ${spin} infinite linear forwards;
 `;
