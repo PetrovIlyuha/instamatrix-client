b7d9751584b89c83cdc108cd5e1c2b06d479511d
Ilya <dnamix1@gmail.com>
2021-04-12 16:32:41 +0300

    add headers to the authLink from apollo

--- src/apollo.js
+++ src/apollo.js
@@ -1,20 +1,51 @@
-import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { AUTH_TOKEN, DARK_THEME } from './constants';
 
-export const loggedIn = makeVar(!!localStorage.getItem('insta-token'));
-export const darkMode = makeVar(false);
+export const loggedIn = makeVar(!!localStorage.getItem(AUTH_TOKEN));
+export const darkMode = makeVar(!!localStorage.getItem(DARK_THEME));
+
+export const enableDarkTheme = () => {
+  localStorage.setItem(DARK_THEME, 'on');
+  darkMode(true);
+};
+
+export const disableDarkTheme = () => {
+  localStorage.removeItem(DARK_THEME);
+  darkMode(false);
+};
 
 export const logUserIn = token => {
-  localStorage.setItem('insta-token', token);
+  localStorage.setItem(AUTH_TOKEN, token);
   loggedIn(true);
 };
 
 export const logUserOut = () => {
-  localStorage.removeItem('insta-token');
+  localStorage.removeItem(AUTH_TOKEN);
   loggedIn(false);
 };
 
-const client = new ApolloClient({
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      auth: token,
+    },
+  };
+});
+
+const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql',
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   connectToDevTools: true,
 });
