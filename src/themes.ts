7562e62bbc91bb4c1e9e3c464e853adad0e10d4a
Ilya <dnamix1@gmail.com>
2021-04-20 15:16:49 +0300

    app themes

--- src/themes.ts
+++ src/themes.ts
@@ -1,22 +1,24 @@
-import { DefaultTheme } from 'styled-components';
+import { DefaultTheme } from "styled-components";
 
 export const lightTheme: DefaultTheme = {
-  background: '#F3F3F3',
-  color: '#0D1117',
-  toggleBorder: 'lightgrey',
-  gradient: 'linear-gradient(#091236, #1E215D)',
+  background: "#F3F3F3",
+  color: "#0D1117",
+  toggleBorder: "lightgrey",
+  gradient: "linear-gradient(#091236, #1E215D)",
   darkTheme: false,
-  logoColor: 'darkblue',
-  boxShadowColor: 'rgba(0,0,0,0.06)',
-  inputBgColor: '#F3F3F3',
+  logoColor: "darkblue",
+  boxShadowColor: "rgba(0,0,0,0.06)",
+  inputBgColor: "#F3F3F3",
+  linkColor: "black",
 };
 export const darkTheme: DefaultTheme = {
-  background: '#0D1117',
-  color: '#F3F3F3',
-  toggleBorder: '#6B8096',
-  gradient: 'linear-gradient(#39598A, #79D7ED)',
+  background: "#0D1117",
+  color: "#F3F3F3",
+  toggleBorder: "#6B8096",
+  gradient: "linear-gradient(#39598A, #79D7ED)",
   darkTheme: true,
-  logoColor: 'lightgreen',
-  boxShadowColor: 'rgba(181, 238, 159, 0.32)',
-  inputBgColor: '#714674',
+  logoColor: "lightgreen",
+  boxShadowColor: "rgba(181, 238, 159, 0.32)",
+  inputBgColor: "#714674",
+  linkColor: "lightgreen",
 };
