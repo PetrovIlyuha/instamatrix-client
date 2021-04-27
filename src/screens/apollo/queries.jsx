9bb9c283f8ba89da3a263bb9c8d6f3313a72e76a
Ilya Petrov <dnamix1@gmail.com>
2021-04-27 16:29:04 +0300

    forgotten queries

---
+++ src/screens/apollo/queries.jsx
@@ -0,0 +1,82 @@
+import gql from "graphql-tag";
+
+export const PHOTO_FEED_QUERY = gql`
+  query observePhotoFeed($page: Int!) {
+    observePhotoFeed(page: $page) {
+      id
+      user {
+        username
+      }
+      file
+      caption
+      isMyPhoto
+      isLikedByMe
+      allLikes {
+        username
+        avatar
+      }
+      user {
+        username
+        avatar
+      }
+      hashtags {
+        hashtag
+      }
+      likesCount
+      comments {
+        content
+        createdByMe
+        user {
+          username
+          avatar
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_USER_PROFILE = gql`
+  query seeProfile($username: String!) {
+    seeProfile(username: $username) {
+      id
+      firstName
+      lastName
+      username
+      email
+      bio
+      avatar
+      isMyProfile
+      isFollowing
+      following {
+        id
+        username
+        avatar
+      }
+      followers {
+        id
+        username
+        avatar
+      }
+      totalFollowing
+      totalFollowers
+      photos {
+        id
+        file
+        caption
+        likesCount
+        hashtags {
+          id
+          hashtag
+        }
+        comments {
+          id
+          content
+          user {
+            username
+            avatar
+          }
+        }
+      }
+    }
+  }
+`;
